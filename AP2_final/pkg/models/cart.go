05b5e7e7ad80c185d90af99041ae687cfb4bd21a
anv01208 <145286694+anv01208@users.noreply.github.com>
2024-06-05 19:34:29 +0500

    Delete AP2_final/pkg directory

--- AP2_final/pkg/models/cart.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Cart struct {
-	Id        int64 `json:"id" gorm:"primaryKey"`
-	Quantity  int32 `json:"quantity"`
-	ProductId int64 `json:"product_id"`
-	UserId    int64 `json:"user_id"`
-}
